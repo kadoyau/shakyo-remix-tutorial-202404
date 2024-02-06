3d6c52003eea68a4dbf13f2d36eae703f6258cc4
kadoyau <11990327+kadoyau@users.noreply.github.com>
2024-02-07 04:07:53 +0900

    Outletを使って子の要素を親にレンダリングする

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import {
   Links,
   LiveReload,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
@@ -23,38 +24,40 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
+      <div id="sidebar">
+        <h1>Remix Contacts</h1>
+        <div>
+          <Form id="search-form" role="search">
+            <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
+            />
+            <div id="search-spinner" aria-hidden hidden={true}/>
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
         </div>
-
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <nav>
+          <ul>
+            <li>
+              <a href={`/contacts/1`}>Your Name</a>
+            </li>
+            <li>
+              <a href={`/contacts/2`}>Your Friend</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet/>
+      </div>
+      <ScrollRestoration/>
+      <Scripts/>
+      <LiveReload/>
       </body>
     </html>
   );
