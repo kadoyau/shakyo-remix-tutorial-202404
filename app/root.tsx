a4873f07f1556e58aa85079488f4340528b80334
kadoyau <11990327+kadoyau@users.noreply.github.com>
2024-02-07 04:15:31 +0900

    fmt

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,7 @@
 import type { LinksFunction } from "@remix-run/node";
 import {
   Form,
-    Link,
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -25,40 +25,40 @@ export default function App() {
         <Links />
       </head>
       <body>
-      <div id="sidebar">
-        <h1>Remix Contacts</h1>
-        <div>
-          <Form id="search-form" role="search">
-            <input
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
-            />
-            <div id="search-spinner" aria-hidden hidden={true}/>
-          </Form>
-          <Form method="post">
-            <button type="submit">New</button>
-          </Form>
-        </div>
-        <nav>
-          <ul>
-            <li>
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
                 <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
+              </li>
+              <li>
                 <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div id="detail">
-        <Outlet/>
-      </div>
-      <ScrollRestoration/>
-      <Scripts/>
-      <LiveReload/>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div id="detail">
+          <Outlet />
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
