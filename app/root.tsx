95bcf79387356ffe1d0e3a150653aa8f73fb11fe
kadoyau <11990327+kadoyau@users.noreply.github.com>
2024-05-24 04:32:34 +0900

    feat: サイドバーのリンクのどこにいるのかわかるようにアクティブなページのリンクに色をつける

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,10 @@
 import { json, LinksFunction, redirect } from "@remix-run/node";
 import {
   Form,
-  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -61,7 +61,12 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -70,7 +75,7 @@ export default function App() {
                         <i>No Name</i>
                       )}{" "}
                       {contact.favorite ? <span>*</span> : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
